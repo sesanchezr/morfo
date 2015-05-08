eda3469a57e0a21410d9677867c6018fcb289c2d
seba <sanchez.sebastian.r@gmail.com>
2015-05-07 22:31:22 -0300

    Sube videos, los lee y guarda la info en nuevo archivo

--- puntos/generar.php
+++ puntos/generar.php
@@ -1,3 +1,14 @@
 <?php 
+	
+ 	/*Hay que validar archivos*/
 	echo "Generando!!! not.";
+	$lswiffy = @fopen($_FILES['lswiffy']['tmp_name'], "r");
+
+	$new_file = fopen("pruebaphp.html", "w+");
+    while (($buffer = fgets($lswiffy, 4096)) !== false) {
+        fwrite($new_file, $buffer);
+    }
+    fclose($lswiffy);
+    fclose($new_file);
+
  ?>
