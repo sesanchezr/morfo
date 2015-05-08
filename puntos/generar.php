92766d7cf09f3439d1ee8a81a9d6c10b96de1434
Maximiliano Castro <macguionbajo@gmail.com>
2015-05-08 16:12:03 -0300

    Implementada funcionalidad formulario puntos

--- puntos/generar.php
+++ puntos/generar.php
@@ -1,4 +1,7 @@
-<?php 
+<?php
+
+	error_reporting(E_ALL);
+	ini_set('error_reporting', E_ALL);
 	function printError($message){
 		die("ERROR: $message");
 	}
@@ -20,27 +23,43 @@
 	$regexp = "/\s+swiffyobject\s=/";
 	$ltext = file_get_contents($lfile['tmp_name']); //contenido del html
 	$lswobj = preg_grep($regexp, explode("\n",$ltext)); // array con swiffycontainer
-	$lswobj = reset($lswobj);
+	$lswobj = reset($lswobj); // "swiffyobject = {assdfasd}"
 	$btext = file_get_contents($bfile['tmp_name']); //contenido del html
 	$bswobj = preg_grep($regexp, explode("\n",$btext)); // array con swiffycontainer
 	$bswobj = reset($bswobj);
 	if ($lswobj=="" || $bswobj==""){
 		printError("Archivo no contiene swiffycontainer");
 	}
+	// renombrar nombre de cada variable swiffyobject
+	$lswiffyvar = "lswiffy";
+	$bswiffyvar = "bswiffy";
+	$lswobj = preg_replace($regexp,"$lswiffyvar =",$lswobj);
+	$bswobj = preg_replace($regexp,"$bswiffyvar =",$bswobj);
 	// Generar .js para cada html con sus swiffycontainers
-	
+	$lfilename = "$lswiffyvar.js";
+	$bfilename = "$bswiffyvar.js";
+	// NO OLVIDAR VALIDAR QUE EL ARCHIVO PUDO SER CREADO, QUE NO EXISTÍA, ETC.
+	if (!($ljsfile = fopen($lfilename,"w"))){
+		printError("No se pudo crear el archivo '$lfilename'");
+	}
+	if (!(fwrite($ljsfile, $lswobj))){
+		printError("No se pudo escribir en el archivo '$lfilename'");
+	}
+	fclose($ljsfile);
+	if (!($bjsfile = fopen($bfilename,"w"))){
+		printError("No se pudo crear el archivo '$bfilename'");
+	}
+	if (!(fwrite($bjsfile, $bswobj))){
+		printError("No se pudo escribir en el archivo '$bfilename'");
+	}
+	fclose($bjsfile);
 	// Generar html con ambos js incluidos	
-
+	  //obtener texto del template
+	$templatefilename = "template_puntos.html";
+	$templatehtml = file_get_contents($templatefilename);
+	  //reemplazar littleswiffy y bigswiffy
+	$templatehtml = preg_replace("/@lswiffy/",$lswiffyvar,$templatehtml);
+	$templatehtml = preg_replace("/@bswiffy/",$bswiffyvar,$templatehtml);
 	// Escupirlos en output
-	
-
-	// $linput = @fopen($_FILES['linput']['tmp_name'], "r");
-
-	// $new_file = fopen("pruebaphp.html", "w+");
- //    while (($buffer = fgets($linput, 4096)) !== false) {
- //        fwrite($new_file, $buffer);
- //    }
- //    fclose($linput);
- //    fclose($new_file);
-
+	echo $templatehtml;
  ?>
