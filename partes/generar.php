02799bf8eef72b2471c9f156a03d924e9d180a45
seba <sanchez.sebastian.r@gmail.com>
2015-05-14 17:51:04 -0300

    Se cambia nombre de swiffy objects y del archivo donde este se guarda

--- partes/generar.php
+++ partes/generar.php
@@ -46,19 +46,29 @@
 		// validacion
 		if ($lswobjs[$i]=="" || $bswobjs[$i]=="")
 			printError("Archivo no contiene swiffycontainer");
+		// Cambio de nombre
+		$lswobj[$i] = preg_replace($regexp,"$swiffylittle".$i."=",$lswobj[$i]);
+		$bswobjs[$i] = preg_replace($regexp,"$swiffybig".$i."=",$bswobjs[$i]);
 	}
+
+	$now = date('d-m-Y/H:i:s'); // Date de ahora para usar de nombre de carpeta
+	$folder = "aristoteles/partes/"; //carpeta donde se ubicarán las partes
+	// DESCOMENTAR LO SIGUIENTE CUANDO SE SUBA A JOOMLA
+	// path = $folder.$now; // carpeta donde se ubicara el actual partes en servidor remoto
+	$path = ""; // carpeta donde se ubicara el actual partes en servidor local
+	mkdir($path, 0755, true);
 
 	// Generar .js para cada html con sus swiffycontainers
 	$lfilenames = array(); $bfilenames = array(); 
 	for ($i=0; $i<$file_count; $i++){
-		$lfilenames[$i] = "$lswiffyvar". $i .".js";
-		$bfilenames[$i] = "$bswiffyvar". $i .".js";
-		if (!($ljsfile = fopen($lfilenames[$i],"w")))
+		$lfilenames[$i] = "$swiffylittle". $i .".js";
+		$bfilenames[$i] = "$swiffybig". $i .".js";
+		if (!($ljsfile = fopen($path.'/'.$lfilenames[$i],"w")))
 			printError("No se pudo crear el archivo .js");
 		if (!(fwrite($ljsfile, $lswobjs[$i])))
 			printError("No se pudo escribir en el archivo '$lfilename'");
 		fclose($ljsfile);
-		if (!($bjsfile = fopen($bfilenames[$i],"w")))
+		if (!($bjsfile = fopen($path.'/'.$bfilenames[$i],"w")))
 			printError("No se pudo crear el archivo '$bfilename'");
 		if (!(fwrite($bjsfile, $bswobjs[$i])))
 			printError("No se pudo escribir en el archivo '$bfilename'");
@@ -68,21 +78,9 @@
 	// Obtenemos el template de los puntitos y linkeamos el .js generado 
 	/* WARNING : Cambiar 'templatefilename_local' POR 'templatefilename_remoto' cuando se suba a Joomla */
 	$templatefilename_local = "template_partes.html";
-	$div_template_local = "div.html"
 	//$templatefilename_remoto = "/home/morfo3/public_html/template_puntos.html";
 	$templatehtml = file_get_contents($templatefilename_local); // <-- ERROR AQUI! NO ESTA ENCONTRANDO EL TEMPLATE, BECAUSE OF REASONS ! 
-	$divtemplate = file_get_contents($div_template_local)
-	$script_tag = "<script type=\"text\/javascript\" src=\"js\/objetos\/@name.js\"><\/script>"
 
-	$alldivs = '' //Aqui guardamos todos los div generados para luego colocarlos en el template
-	$scripts = '' //Aqui colocamos todos los scripts de los swiffy
-	for ($i=0; $i<$file_count; $i++){
-		$this_div =  preg_replace("/@num/",$i,$divtemplate);
-		$alldivs = $alldivs . $this_div;
-		$this_lscript = preg_replace("/@name/", $lfilenames[$i], $divtemplate)
-		$this_bscript =
-		$scripts = $scripts .
-	}
 /*	//reemplazar ubicación de littleswiffy y bigswiffy 
 	$templatehtml = preg_replace("/@lswiffy/",$path.'/'.$lswiffyvar,$templatehtml);
 	$templatehtml = preg_replace("/@bswiffy/",$path.'/'.$bswiffyvar,$templatehtml);
