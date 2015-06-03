0324daba3edc5d941144e7b4c08880fcb20d72b6
sebastian <sebastiansanchez1990@ug.uchile.cl>
2015-06-03 16:28:21 -0300

    Js del menu

---
+++ menu/menu.js
@@ -0,0 +1,27 @@
+jQuery(document).ready(function(){
+	// Tomar menu principal
+	var menu = jQuery('nav')[0].children[0].children;
+	// clono el dropdown activo
+	var clon = jQuery(menu).children('.dropdown').children().children('ul').children('.active').clone();
+	// borro el resto de dropdown
+	jQuery(menu).children('.dropdown').children().children('ul').children().children('.dropdown').remove();
+	// chek si hay dropdown
+	if (jQuery.isEmptyObject(clon)){
+		
+	}
+	else{
+		jQuery('.maintop-1 .block').css('z-index',9999);
+		var i = 1;
+		while(!clon.length==0){
+			jQuery('.maintop-1').append("<div class=\"block widget widget-menu no-title clearfix \" style=\"min-height: 40px;\"><div class=\"content\"> \
+	        <nav class=\"ex-menu hidden-phone\" dropdown-animation=\"scale-up\" dropdown-sub-animation=\"slide-right\">  \
+	        <ul id=\"sub" + i + "\" class=\"l1\"><\/ul><\/nav><\/div>");
+			var clon2 = jQuery(clon).children('.dropdown').children().children('ul').children('.active').clone();
+			jQuery(clon).children('.dropdown').children('.column').children().children().children('.dropdown').remove();
+	        jQuery("#sub" + i).append(jQuery(clon).children('.dropdown').children('.column').children().children());
+	        ++i;
+	        clon = clon2;
+	    } nz
+	}
+
+});
